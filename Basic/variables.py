26971b52e14843e5c7d75674fe8cb901e4c46e53
Vanshikakhasat <vanshikakhasat@gmail.com>
2025-12-22 18:30:02 +0530

    variables and data types example

---
+++ Basic/variables.py
@@ -0,0 +1,9 @@
+name = "Vanshika"
+student_class = 12
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Class:",student_class)
+print("Height:", height)
+print("Is Student:", is_student)
