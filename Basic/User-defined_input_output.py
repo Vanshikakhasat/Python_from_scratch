426216104aa906c88ff8d40d9c26c8f76e022aac
Vanshikakhasat <vanshikakhasat@gmail.com>
2025-12-22 18:42:54 +0530

    User define input output

---
+++ Basic/User-defined_input_output.py
@@ -0,0 +1,9 @@
+# Taking input from user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in feet: "))
+
+print("\n--- User Information ---")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height, "feet")
